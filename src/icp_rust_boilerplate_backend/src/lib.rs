ad00bfca248d4cd9fabb519390f741b269b94b9d
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2024-09-04 12:16:18 +0300

    building icp canister

--- src/icp_rust_boilerplate_backend/src/lib.rs
+++ src/icp_rust_boilerplate_backend/src/lib.rs
@@ -1,4 +1,15 @@
-#[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
-}
+use serde;
+use candid::{ Decode, Encode };
+use ic_cdk::api::time;
+use ic_stable_structures::{
+    memory_manager::{ MemoryId, MemoryManager, VirtualMemory },
+    Cell,
+    DefaultMemoryImpl,
+};
+use std::{ borrow::Cow, cell::RefCell };
+
+// Defining Memory and IdCell
+type Memory = VirtualMemory<DefaultMemoryImpl>;
+type IdCell = Cell<u64, Memory>;
+
+// The IdCell is a cell responsible for holding the current ID of the message. We'll utilize this to generate unique IDs for each message.
